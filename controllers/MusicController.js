d0a73ec4d18dc0cf5adb993587730e512221a4cc
raphaellns <raphaellnunes0307@gmail.com>
2025-12-11 10:10:29 -0300

    fix: Salvar nomes de artes ao invés de IDs no registro de artistas

--- controllers/MusicController.js
+++ controllers/MusicController.js
@@ -95,31 +95,14 @@ export default class MusicController {
                 let filtro = {}
                 if (busca) {
                     const regex = { $regex: busca, $options: 'i' }
-                    const or = [ { arte: regex }, { artista: regex } ]
+                    const or = [ { arte: regex }, { artista: regex }, { artes: regex } ]
                     if (/^\d+$/.test(busca)) {
                         or.push({ idade: Number(busca) })
                     }
                     filtro = { $or: or }
                 }
                 const resultado = await Musica.find(filtro).sort({ createdAt: -1 })
-
-                // Resolver IDs de artes para títulos
-                const artistasComArtes = await Promise.all(resultado.map(async (artista) => {
-                    const artistaObj = artista.toObject()
-                    
-                    if (artistaObj.artes && artistaObj.artes.length > 0) {
-                        try {
-                            const artesTitulos = await Arte.find({ _id: { $in: artistaObj.artes } }).select('titulo')
-                            artistaObj.artes = artesTitulos.map(arte => arte.titulo)
-                        } catch (erro) {
-                            console.error('Erro ao resolver artes:', erro)
-                        }
-                    }
-                    
-                    return artistaObj
-                }))
-
-                res.render(this.caminhoBase + 'lst', { Musicas: artistasComArtes, busca })
+                res.render(this.caminhoBase + 'lst', { Musicas: resultado, busca })
             } catch (err) {
                 console.error('Erro ao listar artistas:', err)
                 res.status(500).send('Erro ao listar artistas')
